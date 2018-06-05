2bcb6bacbfc10db25f6482a1a87216553263fe98
Aliaksei Sasiankou <aliaksei_sasiankou@epam.com>
2018-06-05 16:21:37 +0300

    Configured sslSupport, headless work's fine but browsermob doesn't

--- src/main/java/by/tut/accounttests/util/WebDriverHandler.java
+++ src/main/java/by/tut/accounttests/util/WebDriverHandler.java
@@ -1,49 +1,58 @@
 package by.tut.accounttests.util;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.mitm.CertificateAndKeySource;
+import net.lightbody.bmp.mitm.KeyStoreFileCertificateSource;
+import net.lightbody.bmp.mitm.manager.ImpersonatingMitmManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
 
 public class WebDriverHandler {
 
-	/***
-	 * 
-	 * Static method, allows to download, startup and create instance of the driver
-	 * that we need and what we can choose with help parameters of this method.
-	 * For example, we can get chromedriver putting BrowserType.CHROME into method parameters.
-	 * 
-	 * @param BrowserType type
-	 * @return WebDriver
-	 */
-	public static WebDriver loadDriver(BrowserType type, DesiredCapabilities capabilities) {
-		switch (type) {
-		case FIREFOX:
-			WebDriverManager.firefoxdriver().setup();
-            FirefoxOptions firefoxOptions = new FirefoxOptions();
-            firefoxOptions.merge(capabilities);
-			return new FirefoxDriver(firefoxOptions);
-		case CHROME:
-			WebDriverManager.chromedriver().setup();
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.merge(capabilities);
-			return new ChromeDriver(chromeOptions);
-		case IE:
-			WebDriverManager.iedriver().setup();
-			return new InternetExplorerDriver();
-		case OPERA:
-			WebDriverManager.operadriver().setup();
-			return new OperaDriver();
-		default:
-			WebDriverManager.chromedriver().setup();
-            ChromeOptions defaultChromeOptions = new ChromeOptions();
-            defaultChromeOptions.merge(capabilities);
-			return new ChromeDriver(defaultChromeOptions);
-		}
+    private static BrowserMobProxyServer browserMobProxy;
+
+	public static WebDriver loadDriver(DesiredCapabilities capabilities) {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setHeadless(true);
+        chromeOptions.setAcceptInsecureCerts(true);
+        chromeOptions.addArguments("test-type");
+		chromeOptions.addArguments("--proxy-server='direct://'",
+                "--proxy-bypass-list=*",
+                "--start-maximized",
+               "window-size=1980,960",
+                "--no-sandbox",
+                "--ignore-certificate-errors",
+                "--allow-insecure-localhost");
+        //chromeOptions.addArguments("--acceptSslCerts=true");
+        //chromeOptions.addArguments("--disable-web-security");
+
+        chromeOptions.merge(capabilities);
+		return new ChromeDriver(chromeOptions);
 	}
+
+    public static BrowserMobProxyServer createBrowserMobProxySingleton() {
+
+       CertificateAndKeySource existingCertificateSource =
+               new KeyStoreFileCertificateSource("PKCS12",
+                       new File("./src/test/resources/sslSupport/ca-keystore-rsa.p12"),
+                       "key", "password");
+
+       // configure the MitmManager to use the custom KeyStore source
+        ImpersonatingMitmManager mitmManager = ImpersonatingMitmManager.builder()
+                .rootCertificateSource(existingCertificateSource)
+               .trustAllServers(true)
+                .build();
+
+        browserMobProxy = new BrowserMobProxyServer();
+        browserMobProxy.setMitmManager(mitmManager);
+        browserMobProxy.start(0);
+
+        return browserMobProxy;
+    }
 }
--- src/test/java/by/tut/accounttests/pages/PagesTest.java
+++ src/test/java/by/tut/accounttests/pages/PagesTest.java
@@ -4,10 +4,8 @@ import by.tut.accounttests.data.TestData;
 import by.tut.accounttests.domain.UserAccount;
 import by.tut.accounttests.mailer.JavaMailer;
 import by.tut.accounttests.mailer.Mail;
-import by.tut.accounttests.util.BrowserType;
 import by.tut.accounttests.util.TestLogger;
 import by.tut.accounttests.util.WebDriverHandler;
-import net.lightbody.bmp.BrowserMobProxy;
 import net.lightbody.bmp.BrowserMobProxyServer;
 import net.lightbody.bmp.client.ClientUtil;
 import net.lightbody.bmp.core.har.Har;
@@ -27,17 +25,19 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PagesTest {
 
-	private final static Logger LOGGER = TestLogger.getLogger(PagesTest.class);
+    private final static Logger LOGGER = TestLogger.getLogger(PagesTest.class);
 	
     private static final Mail MAIL = new Mail("Test mail", "Test mail.");
+    private static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd HH-mm-ss";
 
     private Pages pages;
     private WebDriver driver;
-    private BrowserMobProxy proxy;
+    private BrowserMobProxyServer proxy;
     private UserAccount mailer;
     private UserAccount addressee;
     private Har har;
@@ -55,38 +55,44 @@ public class PagesTest {
     	LOGGER.info("Step 1. Send mail to " + addressee.getEmail() + " with Java Mail Api.");
         JavaMailer.sendMail(mailer, addressee, MAIL);
 
-        proxy = new BrowserMobProxyServer();
-        proxy.start(8082);
+        proxy = WebDriverHandler.createBrowserMobProxySingleton();
+      // proxy = new BrowserMobProxyServer();
+      // proxy.start(8082);
+
         Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+//        capabilities.setCapability(CapabilityType.SUPPORTS_NETWORK_CONNECTION, true);
+//        capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 
-        driver = WebDriverHandler.loadDriver(BrowserType.FIREFOX, capabilities);
+        driver = WebDriverHandler.loadDriver(capabilities);
         pages = PageFactory.initElements(driver, Pages.class);
         testContext.setAttribute("WebDriver", this.driver);// set driver for listeners
 
         proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
-        proxy.newHar(testContext.getName());
     }
 
     @Test
     public void shouldCheckMailInSentFolderAndReturnTrue() {
+        proxy.newHar("TestRun - " +  new SimpleDateFormat(DATE_FORMAT_PATTERN).format(new Date()));
+
     	LOGGER.info("Step 2. Load page with url http://mail.tut.by.");
+        proxy.newPage("http://mail.tut.by");
         pages.mailTutByPage().loadPage();
         LOGGER.info("Step 3. Login mailbox with account " + mailer.getEmail());
         pages.mailTutByPage().logIn(mailer);
+        proxy.endPage();
+        proxy.newPage("yandex.by - logged account" + mailer.getEmail());
         LOGGER.info("Step 4. Get into sent folder of account " + mailer.getEmail());
-        har = proxy.getHar();
         pages.mailBoxPage().getIntoSentFolder();
-        har = proxy.getHar();
         LOGGER.info("Step 5. Check sent messages to the account " + addressee.getEmail());
         boolean isSentMail = pages.mailBoxPage().checkMail(addressee, MAIL);
         LOGGER.info("Step 6. Delete all sented messages, clear up for next test class instance.");
         pages.mailBoxPage().deleteMessages();
         LOGGER.info("Step 7. Logout from account " + mailer.getEmail());
         pages.mailBoxPage().logOut();
+        proxy.endPage();
 
-        har = proxy.getHar();
         Assert.assertTrue(isSentMail);
     }
 
@@ -94,16 +100,19 @@ public class PagesTest {
     public void shouldCheckMailInboxFolderAndReturnTrue() {
     	LOGGER.info("Step 8. Load page tiwh url http://mail.tut.by.");
         pages.mailTutByPage().loadPage();
-        har = proxy.getHar();
+        proxy.newPage("http://mail.tut.by");
         LOGGER.info("Step 9. Login mailbox with account " + addressee.getEmail());
         pages.mailTutByPage().logIn(addressee);
-        har = proxy.getHar();
+        proxy.newPage("Logged into mailbox - yandex.by, account " + addressee.getEmail());
         LOGGER.info("Step 10. Check messages in inbox foldr from account " + mailer.getEmail());
         boolean isInboxMail = pages.mailBoxPage().checkMail(mailer, MAIL);
         LOGGER.info("Step 11. Delete all sented messages, clear up for next test class instance.");
         pages.mailBoxPage().deleteMessages();
         LOGGER.info("Step 12. Logout from account " + addressee.getEmail());
         pages.mailBoxPage().logOut();
+        proxy.endPage();
+
+        har = proxy.getHar();
         
         Assert.assertTrue(isInboxMail);
     }
@@ -111,9 +120,9 @@ public class PagesTest {
     @AfterClass(alwaysRun = true)
     public void teardown() {
     	LOGGER.info("Test suite is over!");
-        driver.quit();
 
-        File harFile = new File("target/newharfile.har");
+
+        File harFile = new File("test-output/TestRun - " +  new SimpleDateFormat(DATE_FORMAT_PATTERN).format(new Date()) + ".har");
         try {
             harFile.createNewFile();
             har.writeTo(harFile);
@@ -121,5 +130,6 @@ public class PagesTest {
             LOGGER.error(e);
         }
         proxy.stop();
+        driver.quit();
     }
 }
